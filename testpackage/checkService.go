037f79ef0f184337e3f5dbcd811b6d6b0247eca5
susanta2022 <susanta@adansa.com>
2023-12-17 16:36:00 +0530

    change fro

--- testpackage/checkService.go
+++ testpackage/checkService.go
@@ -6,7 +6,7 @@ import (
 	mysql_services "github.com/susanta2022/rlb-go-service/services"
 )
 
-func check_test_service() {
+func checkTestService() {
 	mysql_services.PrintConnectionStatus()
 	fmt.Print("response from rlb-service-test >>>>")
 }
