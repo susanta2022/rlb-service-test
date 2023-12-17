6d221634ba4cfb9dfbce396fad06aa02e2c89b99
susanta2022 <susanta@adansa.com>
2023-12-17 16:43:24 +0530

    version v0.0.5

--- testpackage/checkService.go
+++ testpackage/checkService.go
@@ -2,11 +2,11 @@ package testpackage
 
 import (
 	"fmt"
-
-	mysql_services "github.com/susanta2022/rlb-go-service/services"
+	// mysql_services "github.com/susanta2022/rlb-go-service/services"
 )
 
 func checkTestService() {
-	mysql_services.PrintConnectionStatus()
+	// mysql_services.PrintConnectionStatus()
+
 	fmt.Print("response from rlb-service-test >>>>")
 }
